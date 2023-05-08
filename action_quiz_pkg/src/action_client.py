60a2bde672c90a78dbb391ad9db2c098dc45554e
alexandru-tech-web <gheorghitaalexandru.tehnic@yahoo.com>
2023-05-08 14:57:32 +0300

    quiz_action

--- action_quiz_pkg/src/action_client.py
+++ action_quiz_pkg/src/action_client.py
@@ -7,24 +7,23 @@ import rospy
 
 
 def drone_control_client():
-    # Initialize the ROS node
+ 
     rospy.init_node('drone_control_client')
 
-    # Create an action client for the DroneControl action
+
     client = actionlib.SimpleActionClient('drone_control', RobAction)
     client.wait_for_server()
 
-    # Create and populate the goal message
+ 
     goal = RobGoal()
     goal.command = "TAKEOFF"
 
-    # Send the goal to the action server
+
     client.send_goal(goal)
 
-    # Wait for the action to complete
     client.wait_for_result()
 
-    # Get the final result of the action
+ 
     result = client.get_result()
 
     if result.status == actionlib.GoalStatus.SUCCEEDED:
